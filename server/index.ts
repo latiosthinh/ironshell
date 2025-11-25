a251f5048208a8f846ef45c20f2f2c590efa9d57
Thomas Nguyen <latiosthinh@live.com>
2025-11-25 17:54:17 +0700

    feat: update default command list

--- server/index.ts
+++ server/index.ts
@@ -232,7 +232,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 50000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
